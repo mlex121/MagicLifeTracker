98c02ee9d60684da823c4bc5c51eff4ab2e267bc
Alexander Lim <me@alexanderlim.com>
2017-11-05 03:18:11 -0800

    Initial commit

---
+++ MagicLifeTracker/ALViewController.m
@@ -0,0 +1,33 @@
+//
+//  ALViewController.m
+//  MagicLifeTracker
+//
+//  Created by Alexander Lim on 2017-11-05.
+//  Copyright 2017 Alexander Lim
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+#import "ALViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ALViewController ()
+
+@end
+
+@implementation ALViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
